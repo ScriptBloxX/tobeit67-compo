c05109811f1dc58d574449f89c07cb001f1d2ebc
9hodev <siaskkh4884@gmail.com>
2023-11-10 08:29:20 +0700

    lactasoy v-1

---
+++ exam/lactasoy.py
@@ -0,0 +1,22 @@
+a = int(input()) #ราคา
+b = int(input()) #จำนวนหลอดที่ต้องใช้
+c = int(input()) #จำนวนราคาที่ลดลงต่อหลอดที่ใช้
+d = int(input()) #จำนวนที่ต้องการ
+
+soy_price = 0
+discount = 0
+l = 0
+
+if b <= 0:
+    soy_price = a*d
+else:
+    if c>a:
+        c=a
+    discount = (d//b)*c
+    soy_price = (a*d)
+    for i in range(d//b):
+        soy_price-=a
+    soy_price += discount
+
+# print(soy_price,discount,a*d)
+print(soy_price)
